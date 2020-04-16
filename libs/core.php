d04f37016267fd504d2cbefeadf26e329af2e967
Oliver Guo <oliguo@users.noreply.github.com>
2020-04-16 16:56:27 +0800

    Create core.php

---
+++ libs/core.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Get the 'STRIPE_SECRET_KEY' and 'STRIPE_PUBLISHABLE_KEY' from
+ * https://dashboard.stripe.com/test/apikeys After you login
+ * 
+ * Get the 'STRIPE_WEBHOOK_SECRET' from
+ * https://dashboard.stripe.com/test/webhooks After you login
+ */
+define("STRIPE_SECRET_KEY","sk_test_xxx");
+define("STRIPE_PUBLISHABLE_KEY","pk_test_xxx");
+define("STRIPE_WEBHOOK_SECRET","whsec_xxx");
